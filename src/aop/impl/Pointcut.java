749ac92bfae1d0abd90b7cd6ad915b270eb0b12a
zexho <zouzhihao994@gmail.com>
2021-08-07 13:15:07 +0800

    [update]: 添加测试类

--- src/aop/impl/Pointcut.java
+++ src/aop/impl/Pointcut.java
@@ -10,4 +10,8 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Pointcut {
+
+    // 使用beanName定义切点
+    String beanName();
+
 }
---
+++ src/aop/sample/TestClass.java
@@ -0,0 +1,13 @@
+package aop.sample;
+
+/**
+ * @author Zexho
+ * @date 2021/8/7 10:53 上午
+ */
+public class TestClass {
+
+    public void foo() {
+        System.out.println("test class -> foo()");
+    }
+
+}
